4fe17f2c0670b378615e6102bd990416df335b40
Balazs Szele <balazs.szele@prezi.com>
2014-09-20 17:06:18 +0200

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- player.rb
+++ player.rb
@@ -90,17 +90,17 @@ class Player
     h.each do |k, v|
       p 'k, v   ', k, v
       if v == 2
-        pair == true
-        # if k == "A" or k == "K" or k == "Q" or k == "J"
-        #   high_pair = true
-        # end
-        if pair == true
-          two_pair == true
+        pair = true
+        if k == "A" or k == "K" or k == "Q" or k == "J"
+          high_pair = true
+        end
+        if pair == true or high_pair
+          two_pair = true
         end
       elsif v == 3
-        three == true
+        three = true
       elsif v == 4
-        four == true
+        four = true
       end 
     end
 
