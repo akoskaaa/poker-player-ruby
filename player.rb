722b2bc030ac1b09e2d5f108a2da32e40b642a65
Balazs Szele <balazs.szele@prezi.com>
2014-09-20 11:35:27 +0200

    evaluate cards

--- player.rb
+++ player.rb
@@ -18,4 +18,32 @@ class Player
   def showdown(game_state)
 
   end
+
+  def evaluate_card(cards):
+    h = Hash.new(0)
+    high_card = false
+    pair = false
+    three = false
+    cards.each do |c|
+      if c == "A" or c == "K" or c == "Q" or c == "J"
+        high_card = true
+      end 
+      h[c] +=1
+    end
+    h.each do |k, v|
+      if v == 2
+        pair == true
+      elsif v == 3
+        three == true
+      end 
+    end
+    if high_card or pair or three
+      return 1 
+    else
+      return 0
+    end
+
+
+
+  end
 end
