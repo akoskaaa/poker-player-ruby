aeab2515fa60b906e07bf62ad1eb6f29da0dd83e
Balazs Szele <balazs.szele@prezi.com>
2014-09-20 15:30:41 +0200

    More debug

--- player.rb
+++ player.rb
@@ -14,7 +14,7 @@ class Player
     cards = cardhelper.get_cards_from_state(game_state)
     rank = evaluate_card(cards)
     p '##########'
-    p 'rank', rank, 'call', call, 'stack', game_state['players'][5]['stack']
+    p 'rank', rank, 'call', call, 'stack', game_state['players'][5]['stack'], 'cards', cards
     p '##########'
     if rank >= 4
       bet = game_state['players'][5]['stack']
