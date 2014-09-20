94d00b77e9b49949dde013c282dc3a7fc49b6e49
Balazs Szele <balazs.szele@prezi.com>
2014-09-20 13:58:55 +0200

    Debug

--- player.rb
+++ player.rb
@@ -11,6 +11,11 @@ class Player
     cardhelper = CardHelpers.new
     cards = cardhelper.get_cards_from_state(game_state)
     rank = evaluate_card(cards)
+    p '##########'
+    p rank
+    p '##########'
+    p call
+    p '##########'
     if rank >= 3
       bet = game_state['players'][5]['stack']
     elsif rank == 2
